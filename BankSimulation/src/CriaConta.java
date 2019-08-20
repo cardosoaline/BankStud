3fc731e8370721401fb0834df9c93f14170bfe7d
cardosoalineee <alinedsc1989@gmail.com>
2019-08-20 15:51:52 -0300

    Acrescentando Atributos e metodos

---
+++ BankSimulation/src/CriaConta.java
@@ -0,0 +1,15 @@
+
+public class CriaConta {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Conta primeiraConta = new Conta();
+		
+		primeiraConta.saldo = 200;
+		
+		System.out.println(primeiraConta.saldo);
+
+	}
+
+}
