201a099ae76ce270ffecb2028e1237ff018e2f1a
cardosoalineee <alinedsc1989@gmail.com>
2019-08-20 16:08:28 -0300

    Acrescentado uma Segunda conta para testes

--- BankSimulation/src/CriaConta.java
+++ BankSimulation/src/CriaConta.java
@@ -5,10 +5,17 @@ public class CriaConta {
 		// TODO Auto-generated method stub
 		
 		Conta primeiraConta = new Conta();
+		Conta segundaConta = new Conta();
 		
-		primeiraConta.saldo = 200;
+		primeiraConta.saldo = 200;		
+		primeiraConta.saldo += 100;		
 		
-		System.out.println(primeiraConta.saldo);
+		segundaConta.saldo = 50;
+		
+		
+		System.out.println("A primeira conta está com saldo de: R$"+ primeiraConta.saldo);
+		System.out.println(" A segunda conta está com saldo atual de: R$"+segundaConta.saldo);
+		
 
 	}
 
