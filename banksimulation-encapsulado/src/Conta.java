25230f18742bb45727f2e0a3df90ccf8434978a0
cardosoalineee <alinedsc1989@gmail.com>
2019-08-29 16:33:52 -0300

    Penúltimo capitulo do curso Construtor

--- banksimulation-encapsulado/src/Conta.java
+++ banksimulation-encapsulado/src/Conta.java
@@ -75,6 +75,16 @@ public class Conta {
 	 public void setAgencia(int agencia) {
 		this.agencia = agencia;
 	}
+	 
+	 public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+	 
+	 public Cliente getTitular() {
+		return titular;
+	}
+	 
+	 //Testando commitar no git hub
 }
 
 
--- banksimulation-encapsulado/src/TestaGeteSet.java
+++ banksimulation-encapsulado/src/TestaGeteSet.java
@@ -8,9 +8,29 @@ public class TestaGeteSet {
 		
    // O encapsulamento ajuda a validar na propria classe auxilia
 		//na parte de manipular as informações.
-		  conta.setNumero(1334);
+		 // conta.setNumero(1334);
 		  System.out.println(conta.getNumero());
-
-
+		  
+		  Cliente aline = new Cliente ();
+		  
+		  //conta.titular=paulo; este processo não pode pois se encontra em private.
+		  
+		  conta.setTitular(aline);
+		  
+		  aline.setNome("Aline Lopes"); 
+		   
+		  
+		  conta.getTitular().setProfissao("Programadora");		  
+		
+		  Cliente titularDaConta = conta.getTitular();
+		  titularDaConta.setProfissao("Programadora");
+		  
+		  System.out.println(titularDaConta);
+		  System.out.println(aline);
+		  System.out.println(conta.getTitular());
+		  
+		  
+		  
+		  
 	}
 }
